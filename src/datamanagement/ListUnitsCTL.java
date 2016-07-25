8ae9540fc38925cf42530f727a69ccc001356051
LoicNyssen <loic@nyssen.com.au>
2016-07-25 15:45:12 +0200

    Refactored ListUnitsCTL.java

--- src/datamanagement/ListUnitsCTL.java
+++ src/datamanagement/ListUnitsCTL.java
@@ -1,10 +1,20 @@
 package datamanagement;
-public class ListUnitsCTL {
-    private UnitManager um;
-public ListUnitsCTL() {
-        um = UnitManager.UM();
+
+public class ListUnitsCTL
+{
+	private UnitManager unitManager_;
+	
+	public ListUnitsCTL()
+	{
+		unitManager_ = UnitManager.unitManager();
+	}
+	
+	public void listUnits( IUnitLister lister )
+	{
+		lister.clearUnits();
+		UnitMap units = unitManager_.getUnits();
+		
+		for (String s : units.keySet() )
+			lister.addUnit(units.get(s));
+	}
 }
-            public void listUnits( IUnitLister lister ) {
-lister.clearUnits();UnitMap units = um.getUnits();
-        for (String s : units.keySet() )
-            lister.addUnit(units.get(s));}}
